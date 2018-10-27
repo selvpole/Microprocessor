c5dcc5690f78a776c6ca43aeb3afaba7ad74ebbe
Pole <david969698@gmail.com>
2018-10-27 13:03:05 +0800

    Update lab3-1/main.s

--- lab3-1/main.s
+++ lab3-1/main.s
@@ -21,59 +21,59 @@ main:
 	movs r4, #0
 	movs r8, #10
 postfix:
-	add		r1, r1, #1
-	ldrb	r3, [r1] 	// r3 = str[i]
-	cmp		r3, 0x2B	// if(str[i] == "+") => plus
-	beq		add
-	cmp		r3, 0x2D	// if(str[i] == "-") => minus
-	beq		check
-	cmp 	r3, 0x20	// if(str[i] == " ") => push num
-	beq 	push
-	b 		atoi			// else atoi
+	add r1, r1, #1
+	ldrb r3, [r1] 	// r3 = str[i]
+	cmp r3, 0x2B	// if(str[i] == "+") => plus
+	beq add
+	cmp r3, 0x2D	// if(str[i] == "-") => minus
+	beq check
+	cmp r3, 0x20	// if(str[i] == " ") => push num
+	beq push
+	b atoi			// else atoi
 check:
-	add 	r2, r1, #1	// r2 = i+1
-	ldrb	r2, [r2]	// r2 = str[i+1]
-	cmp		r2, 0x20	// if(str[i+1] == " ")
-	beq 	sub			//	str[i] = sub
-	b 		neg			// else	str[i] = neg
+	add r2, r1, #1	// r2 = i+1
+	ldrb r2, [r2]	// r2 = str[i+1]
+	cmp r2, 0x20	// if(str[i+1] == " ")
+	beq sub			//	str[i] = sub
+	b neg			// else	str[i] = neg
 add:
-	pop		{r5, r6}
-	add 	r5, r5, r6
-	push 	{r5}
-	movs	r5, #0
-	add 	r1, r1, #1
-	ldrb	r2, [r1]	// r2 = str[i+1]
-	cmp		r2, 0x20	// if(str[i+1] == " ")
-	beq		postfix
+	pop {r5, r6}
+	add r5, r5, r6
+	push {r5}
+	movs r5, #0
+	add r1, r1, #1
+	ldrb r2, [r1]	// r2 = str[i+1]
+	cmp r2, 0x20	// if(str[i+1] == " ")
+	beq postfix
 	b store
 sub:
-	pop 	{r5, r6}
-	sub 	r5, r6, r5
-	push 	{r5}
-	movs	r5, #0
-	add 	r1, r1, #1
-	ldrb	r2, [r1]	// r2 = str[i+1]
-	cmp		r2, 0x20	// if(str[i+1] == " ")
-	beq		postfix
+	pop {r5, r6}
+	sub r5, r6, r5
+	push {r5}
+	movs r5, #0
+	add r1, r1, #1
+	ldrb r2, [r1]	// r2 = str[i+1]
+	cmp r2, 0x20	// if(str[i+1] == " ")
+	beq postfix
 	b store
 atoi:
-	mul 	r5, r5, r8	// num = num*10
-	sub 	r3, r3, #48
-	add		r5, r5, r3
-	b		postfix
+	mul r5, r5, r8	// num = num*10
+	sub r3, r3, #48
+	add r5, r5, r3
+	b postfix
 neg:
-	movs 	r5, #-1
-	sub		r2, r2, 48
-	mul		r5, r5, r2
-	add 	r1, r1, #1
-	b 		postfix
+	movs r5, #-1
+	sub r2, r2, 48
+	mul r5, r5, r2
+	add r1, r1, #1
+	b postfix
 push:
-	push 	{r5}
-	movs	r5, #0
-	b 		postfix
+	push {r5}
+	movs r5, #0
+	b postfix
 store:
-	ldr 	r0, =expr_result
-	pop 	{r5}
-	str		r5, [r0]
+	ldr r0, =expr_result
+	pop {r5}
+	str r5, [r0]
 program_end:
-	B 		program_end
+	B program_end
