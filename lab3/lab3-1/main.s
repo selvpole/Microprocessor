4e40a8ff5944b199d77c18019dd474302da1336b
Pole <david969698@gmail.com>
2018-11-07 09:17:29 +0800

    Update lab3/lab3-1/main.s

--- lab3/lab3-1/main.s
+++ lab3/lab3-1/main.s
@@ -9,71 +9,67 @@
 	postfix_expr:	.asciz "-100 10 20 + - 10 +"
 	.align 0x2
 
-	// "+" = 0x2B
-	// "-" = 0x2D
-	// " " = 0x20
-	// "/0" = 0x00
 main:
 	ldr r0, =postfix_expr
 	ldr sp, =user_stack
-	add sp, sp, #128
-	add r1, r0, #-1	// r1 = counter
-	movs r4, #0
+	add sp, #128
+	add r1, r0, #-1
+	movs r3, #0
 	movs r8, #10
 postfix:
 	add r1, r1, #1
-	ldrb r3, [r1] 	// r3 = str[i]
-	cmp r3, 0x2B	// if(str[i] == "+") => plus
-	beq add
-	cmp r3, 0x2D	// if(str[i] == "-") => minus
+	ldrb r2, [r1]
+	cmp r2, 0x2b	// +
+	beq plus
+	cmp r2, 0x2d	// -
 	beq check
-	cmp r3, 0x20	// if(str[i] == " ") => push num
+	cmp r2, 0x20	// " "
 	beq push
-	b atoi			// else atoi
-check:
-	add r2, r1, #1	// r2 = i+1
-	ldrb r2, [r2]	// r2 = str[i+1]
-	cmp r2, 0x20	// if(str[i+1] == " ")
-	beq sub			//	str[i] = sub
-	b neg			// else	str[i] = neg
-add:
+atoi:
+	mul r3, r8
+	sub r2, r2, #48
+	add r3, r3, r2
+	b postfix
+plus:
 	pop {r5, r6}
 	add r5, r5, r6
 	push {r5}
-	movs r5, #0
 	add r1, r1, #1
-	ldrb r2, [r1]	// r2 = str[i+1]
-	cmp r2, 0x20	// if(str[i+1] == " ")
+	ldrb r2, [r1]
+	cmp r2, 0x20
 	beq postfix
 	b store
+	b postfix
+check:
+	add r1, r1, #1
+	ldrb r2, [r1]
+	cmp r2, 0x20
+	beq sub
+	cmp r2, 0x00
+	beq sub
+neg:
+	movs r3, #-1
+	sub r2, r2, #48
+	mul r2, r3
+	b postfix
+
 sub:
 	pop {r5, r6}
-	sub r5, r6, r5
+	sub r5, r5, r6
 	push {r5}
-	movs r5, #0
-	add r1, r1, #1
-	ldrb r2, [r1]	// r2 = str[i+1]
-	cmp r2, 0x20	// if(str[i+1] == " ")
+	cmp r2, 0x20
 	beq postfix
 	b store
-atoi:
-	mul r5, r5, r8	// num = num*10
-	sub r3, r3, #48
-	add r5, r5, r3
-	b postfix
-neg:
-	movs r5, #-1
-	sub r2, r2, 48
-	mul r5, r5, r2
-	add r1, r1, #1
-	b postfix
 push:
-	push {r5}
-	movs r5, #0
+	push {r3}
+	movs r3, #0
 	b postfix
+
 store:
-	ldr r0, =expr_result
 	pop {r5}
+	ldr r0, =expr_result
 	str r5, [r0]
-program_end:
-	B program_end
+L:
+	b L
+
+
