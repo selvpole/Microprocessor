01f3ae9553eb4c67f73103d3a687c37dbbf96cc5
Pole <david969698@gmail.com>
2018-11-21 19:22:47 +0800

    Update main.s

--- Midtern1/3_Postfix _with_float/main.s
+++ Midtern1/3_Postfix _with_float/main.s
@@ -11,19 +11,54 @@
 	.global main
 
 main:
+	bl init
+	bl push_val
+	ldr r0, =operators
+	mov r1, #0
+	b cal
+push_val:
 	ldr sp, =operand_stack_buttom
 	add sp, #12
 	ldr r0, =operand_stack_pointer
-	vldr.F32 s1, =operand_stack_pointer
-	vpush {s1}
-	add r0, #1
 	vldr s0, [r0]
-	vpush {s0}
-	add r0, #1
-	vldr s0, [r0]
-	vpush {s0}
+	vldr s1, [r0,#4]
+	vldr s2, [r0,#8]
+	vpush {s0,s1,s2}
 	add r1, r0, #-1
-	ldr r0, =operators
-
-postfix:
-
+	bx lr
+cal:
+	vpop {s0, s1}
+	ldrb r2, [r0,r1]
+	cmp r2, 0x2B // +
+	beq add
+	cmp r2, 0x2A // *
+	beq mul
+check:
+	add r1, #1
+	cmp r1, #2
+	bne cal
+	vpop {s0}
+	b program_end
+add:
+	vadd.f32 s0, s1
+	vpush {s0}
+	b check
+mul:
+	vmul.f32 s0, s1
+	vpush {s0}
+	b check
+program_end:
+	b program_end
+init:
+	LDR.W R0, =0xE000ED88
+	//Read CPACR
+	LDR R1, [R0]
+	//Set bits 20-23 to enable CP10 and CP11 coprocessors
+	ORR R1, R1, #(0xF << 20)
+	//Write back the modified value to the CPACR
+	STR R1, [R0]
+	// wait for store to complete
+	DSB
+	// reset pipeline now the FPU is enabled
+	ISB
+	bx lr
